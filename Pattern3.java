40df08b9d8704dc1448f555d6cbd7ff92f39e014
saisri-02 <71486510+saisri-02@users.noreply.github.com>
2021-07-01 22:23:45 +0530

    initial commit

---
+++ Pattern3.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Pattern3
+
+{
+
+public static void main (String[] args) 
+{
+    
+int n,i,j;
+    
+Scanner sc=new Scanner(System.in);
+    
+System.out.println("Enter n value : ");
+    
+n=sc.nextInt();
+    
+char ch='A';
+    
+for(i=1;i<=n;i++)
+    
+{
+        
+for(j=1;j<=i;j++)
+        
+{
+            
+System.out.println(ch+" ");
+            
+ch++;
+        
+}
+        
+System.out.println( );
+    
+}
+    
+
+}
+
+}
